4f5ed02e4aea5efeabc54a638b7499059cabb1de
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2014-11-04 14:12:55 -0800

    Refactoring

--- DorTaxInfoImporter.py
+++ DorTaxInfoImporter.py
@@ -36,6 +36,13 @@ def get_rates_zip_path(the_date=None):
     """
     return "http://dor.wa.gov/downloads/Add_Data/Rates%sQ%s.zip" % get_quarter(the_date)
 
+def get_location_code_boundaries_zip_path(the_date=None):
+    """Gets the path to the ZIP archive containing 
+    """
+    # http://dor.wa.gov/downloads/LocBounds/LOCCODE_PUBLIC_14Q4.zip
+    year, quarter = get_quarter(the_date)
+    return "http://dor.wa.gov/downloads/LocBounds/LOCCODE_PUBLIC_%sQ%s.zip" %  (str(year)[-2:], quarter)
+
 def set_dict_types(d):
     """When CSVs are parsed all fields are interpreted as strings.
     This function sets the values to the correct types.
@@ -48,20 +55,12 @@ def set_dict_types(d):
     # Delete the blank entry
     del d['']
 
-
-
-zip_path = get_rates_zip_path()
-
-
-zip_fn = os.path.split(zip_path)[1]
-
-csv_fn = os.path.splitext(zip_fn)[0] + ".csv"
-
-# Download ZIP and extract the CSV file.
-if not os.path.exists(csv_fn):
-    # Download the zip file and save a local version
-    zip = urllib2.urlopen(zip_path)
-    with open(zip_fn, "wb") as f:
+def download_binary_file(url, out_path):
+    """Downloads a ZIP file from the internet
+    """
+     # Download the zip file and save a local version
+    zip = urllib2.urlopen(url)
+    with open(out_path, "wb") as f:
         while 1:
             packet = zip.read()
             if not packet:
@@ -69,16 +68,39 @@ if not os.path.exists(csv_fn):
             f.write(packet)
         f.close()
 
-    with zipfile.ZipFile(zip_fn) as zf:
-        zf.extractall(".")
-
-out_list = {}
-
-with open(csv_fn, 'rb') as csvfile:
+def iterate_rate_csv(csvfile):
+    """Generator function that iterates over a CSV file using a csv.DictReader.
+    Additionally converts the types from string when necessary.
+    """
     reader = csv.DictReader(csvfile)
     for d in reader:
         set_dict_types(d)
-        out_list[d["Code"]] = d
+        yield d
 
-with open("rates.json", "w") as jsonfile:
-    json.dump(out_list, jsonfile, cls=DateEncoder)
+
+if __name__ == "__main__":
+    zip_path = get_rates_zip_path()
+
+
+    zip_fn = os.path.split(zip_path)[1]
+
+    csv_fn = os.path.splitext(zip_fn)[0] + ".csv"
+
+    # Download ZIP and extract the CSV file.
+    if not os.path.exists(zip_fn):
+        download_binary_file(zip_path, zip_fn)
+
+    if not os.path.exists(csv_fn):
+        with zipfile.ZipFile(zip_fn) as zf:
+            zf.extractall(".")
+
+    out_list = {}
+
+
+    with open(csv_fn, 'rb') as csvfile:
+        for d in iterate_rate_csv(csvfile):
+            out_list[d["Code"]] = d
+
+    with open("rates.json", "w") as jsonfile:
+        json.dump(out_list, jsonfile, cls=DateEncoder)
+
