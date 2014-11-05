bb7f537cdcc339d16209c12e8985e98ebecc87a6
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2014-11-05 09:38:25 -0800

    Updated setup to dl gpgk dll.
    Setup now downloads SQLite GeoPackage extension library (zipped).
    Renamed DorTaxInfoImporter.py to dortax.py

--- setup.py
+++ setup.py
@@ -4,23 +4,25 @@ import sys, os, urllib2
 
 get_pip_url = "https://bootstrap.pypa.io/get-pip.py"
 
-def download_binary_file(url, out_path):
-    """Downloads a ZIP file from the internet
+def download_binary_file(url, out_path=None):
+    """Downloads a ZIP file from the internet if it does not already exist.
     """
-     # Download the zip file and save a local version
-    zip = urllib2.urlopen(url)
-    with open(out_path, "wb") as f:
-        while 1:
-            packet = zip.read()
-            if not packet:
-                break
-            f.write(packet)
-        f.close()
+    if not out_path:
+        url_root, out_path = os.path.split(url)
+        del url_root
+    if not os.path.exists(out_path):
+         # Download the zip file and save a local version
+        zip = urllib2.urlopen(url)
+        with open(out_path, "wb") as f:
+            while 1:
+                packet = zip.read()
+                if not packet:
+                    break
+                f.write(packet)
+            f.close()
+    return out_path
 
-get_pip_path = "get-pip.py"
-
-if not os.path.exists(get_pip_path):
-    download_binary_file(get_pip_url, get_pip_path)
+get_pip_path = download_binary_file(get_pip_url)
 
 # Install pip.
 os.system(get_pip_path)
@@ -33,3 +35,6 @@ if not os.path.exists(pip_exe_path):
 
 # Install requirements.
 os.system("%s install -r requirements.txt" % pip_exe_path)
+
+print "Get Geopackage library for SQLite..."
+libgpkg_path = download_binary_file("https://bitbucket.org/luciad/libgpkg/downloads/libgpkg-0.9.18-win64.zip")
